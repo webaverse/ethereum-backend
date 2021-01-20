94f0bc7d4f2c2c45e4c2f98ef11ccdfa2e7a9c40
Avaer Kazmer <a@modules.io>
2021-01-20 04:24:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ cpSidechain.stderr.pipe(fs.createWriteStream('./rinkeby-stderr.log', {
 }));
 
 // geth --datadir mainnet init genesis-mainnet.json
-// cp ./static-nodes-mainnet.json ./rinkeby/static-nodes.json
+// cp ./static-nodes-mainnet.json ./mainnet/static-nodes.json
 // cp ./account-mainnet.json ./mainnet/keystore/UTC--2021-01-20T03-14-48.452051307Z--aae22cabdb635d6bfa1f6d19f921c783c90540c2
 // geth --datadir mainnet --http --http.addr 172.31.2.5 --http.corsdomain '*' --mine --minerthreads 1 --miner.gasprice 0 --targetgaslimit '1000000000' --syncmode full --networkid 1338 --etherbase '0xaae22cabdb635d6bfa1f6d19f921c783c90540c2' --allow-insecure-unlock --unlock '0xaae22cabdb635d6bfa1f6d19f921c783c90540c2' --password ./password
 const cpMainnet = childProcess.spawn('geth', [
